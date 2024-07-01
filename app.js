f03421c753d08b21e7efdf698bd04ee33ea0e93e
Wis3477 <mabohlalenkuna@gmail.com>
2024-07-01 21:57:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function PalindromeChecker(strng) {
+    for (let i = 0; i < strng.length / 2; i++) {
+        if (strng[i] !== strng[strng.length - 1 - i]) { // Corrected index calculation
+            return false;
+        }
+    }
+    return true;
+}
+
+function enterInput() {
+    let userInput = prompt("Enter an input").toLowerCase();
+    if (userInput === null || userInput === "") {
+       console.log("Invalid input");
+        
+    }
+    //userInput = userInput
+    console.log(PalindromeChecker(userInput));
+}
+
+enterInput(); 
